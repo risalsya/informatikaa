002396de4f7813304ece4b083805bad73274b84a
risalsya <145945800+risalsya@users.noreply.github.com>
2024-08-31 13:00:18 +0700

    Add files via upload

---
+++ #penulisan statement.py
@@ -0,0 +1,19 @@
+#penulisan statement
+print("Hello World")
+
+#penulisan string
+Judul = "Jalan ke Borobudur"
+print(Judul)
+
+#penulisan case
+method_name = "Aulia Salsabila"
+print(method_name)
+
+#penulisan blok program
+a = 35
+b = 55
+if b > a:
+    print("b is greater than a")
+
+#penulisan komentar
+"""Menggunakan Triple Tanda Petik"""
