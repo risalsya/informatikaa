037df1cdf922ec926c0eefa7bed55d50fd3ac977
risalsya <145945800+risalsya@users.noreply.github.com>
2024-08-31 14:11:53 +0700

    Add files via upload

---
+++ tugas2.py
@@ -0,0 +1,50 @@
+variabel_ku = "ini isi variabel"
+variabel2 = 20
+print(variabel_ku)
+print(variabel2)
+
+nama = "python"
+print ("nama")
+del(nama)
+print ("nama")
+nama_ku = "Aulia Salsabila"
+umur = 17
+tinggi = 165
+print(nama_ku)
+print(umur)
+print(tinggi)
+harga = 20000 #tipe int
+berat = 25 #float
+jarak = 3e3 #float 3000.0, huruf e artinya exponen 10
+print(harga)
+print(berat)
+print(jarak)
+nama = "salsabila"
+jenis_kelamin = 'P'
+alamat = """
+Dlinggo, Ngadirejo, Tegalrejo,
+Magelang, Jawa Tengah
+"""
+agama = 'islam'
+print(nama)
+print(jenis_kelamin)
+print(alamat)
+print(agama)
+nama="Aulia Salsabila"
+alamat= 'magelang'
+umur = 17
+tinggi = 165
+menikah = false
+print(nama)
+print(alamat)
+print(umur)
+print(tinggi)
+print(menikah)
+a = 10
+b = 3
+c = a / b
+print (c)
+nama_variabel = input("Sebuah Teks")
+print(nama_variabel)
+nama = "Aulia Salsabila"
+print(nama)
